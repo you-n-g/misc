1f429f92de5d00fbc797b6b3f13384efdb8f53e9
Young <afe.young@gmail.com>
2023-06-07 07:52:48 +0000

    Update simple feature

--- src/misc/cli.py
+++ src/misc/cli.py
@@ -1,5 +1,9 @@
 """Command Line Interface."""
 import typer
+import wan
+import time
+from pathlib import Path
+from loguru import logger
 
 app = typer.Typer()
 
@@ -9,8 +13,18 @@ def run() -> None:
     """Run command."""
 
 
-typer_click_object = typer.main.get_command(app)
+@app.command()
+def check_health(p: Path) -> None:
+    # the file should be edited in last 5 hours
+    if p.stat().st_mtime < time.time() - 5 * 3600:
+        info = f"{p} is not edited for at least 5 hours"
+        logger.info(info)
+        wan.ntf(info)
+    else:
+        logger.info("It is edited recently")
 
+
+typer_click_object = typer.main.get_command(app)
 
 if __name__ == "__main__":
     app()  # pragma: no cover
